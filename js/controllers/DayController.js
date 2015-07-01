fbdb2fecb11c3bb3a5c8632e7510e0ff1f779dc8
Sunny Lam <sunny.lam@gmail.com>
2015-07-01 10:39:25 -0400

    modified day controller to use events service

--- js/controllers/DayController.js
+++ js/controllers/DayController.js
@@ -1,3 +1,7 @@
-app.controller('DayController', ['$scope', function($scope) {
-
+app.controller('DayController', ['$scope', 'events', function($scope, events) {
+	// don't forget to change 'forecast.success' to 'SERVICENAME.success'
+	// don't forget to include this as a script element in your index.html file or html file
+  events.success(function(data) {
+    $scope.day = data;
+  });
 }]);
